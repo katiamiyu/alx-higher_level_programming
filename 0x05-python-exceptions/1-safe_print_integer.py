ab2f7f0912e3f353e904ea12eeb26c52c0fee691
katiamiyu <katiamiyu@gmail.com>
2023-08-28 16:37:00 +0000

    udated task 2 chmod

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 
 def safe_print_integer(value):
     try:
-        print("{:d}".format())
+        print("{:d}".format(value))
         return (True)
     except (ValueError, TypeError):
         return (False)
