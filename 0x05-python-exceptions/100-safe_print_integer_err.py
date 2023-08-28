ebba965417363d97c8aa3a667171977c6facc1b5
katiamiyu <katiamiyu@gmail.com>
2023-08-28 20:21:39 +0000

    task 100 pycodestyled

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from sys import stderr
+
+
+def safe_print_integer_err(value):
+    flag = none
+    try:
+        print("{:d}".format(value))
+        flag = True
+    except (TypeError as te):
+        flag = False
+        stderr.write("exception: {}\n".format(te))
+    return flag
