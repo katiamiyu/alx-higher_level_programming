a8ebbbd013190e5fc56fbb8f794550e36034fd2e
katiamiyu <katiamiyu@gmail.com>
2023-08-28 16:07:50 +0000

    pycodestyle check

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+
 def safe_print_list(my_list=[], x=0):
     int i = 0
     while i < x:
@@ -6,7 +8,6 @@ def safe_print_list(my_list=[], x=0):
             print(mylist[i], end='')
         except IndexError:
             break
-    i++;
-
-print("")
-return (i)
+    i++
+    print("")
+    return (i)
