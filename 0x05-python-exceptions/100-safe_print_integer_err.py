c5bd574fa06aa16d72117d77bdcacb95ee30f1b3
katiamiyu <katiamiyu@gmail.com>
2023-08-28 20:45:27 +0000

    advance task 0 and 2

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,6 +7,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except (TypeError, ValueError) as te:
+        stderr.write("exception: {}".format(te))
         return False
-        stderr.write("exception: {}\n".format(te))
     return True
