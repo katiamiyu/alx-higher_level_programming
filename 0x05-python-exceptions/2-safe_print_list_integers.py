f12ad93c7813591993e585f698a122d5176d5d4d
katiamiyu <katiamiyu@gmail.com>
2023-08-28 17:00:24 +0000

    udated task 3 v2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,10 +7,10 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             n += 1
-        except (ValueError, TypeError):
-            pass
         except IndexError:
             break
+        except (ValueError, TypeError):
+            pass
         i += 1
     print("")
     return (n)
