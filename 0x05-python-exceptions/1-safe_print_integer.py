9e0a996ce130f0ccad46099e7ccd0637b1450be4
katiamiyu <katiamiyu@gmail.com>
2023-08-28 16:29:58 +0000

    updated task 2

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        value/1
-    except Exception as e:
+        print("{:d}".format())
+    except (ValueError, TypeError):
         return (False)
     else:
         return (True)
