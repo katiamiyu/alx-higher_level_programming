364b1237f65dbf4a660b71f3f4b161302040c8e0
katiamiyu <katiamiyu@gmail.com>
2023-08-28 20:31:11 +0000

    task 100 pycodestyled

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,6 +7,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except (TypeError, ValueError) as te:
-        return False
         stderr.write("exception: {}\n".format(te))
+        return False
     return True
