758b76fb3e2ff78a716a6136700c9d90ade76baf
katiamiyu <katiamiyu@gmail.com>
2023-08-28 16:14:10 +0000

    updated task 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,9 +5,9 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     while i < x:
         try:
-            print(mylist[i], end='')
+            print(my_list[i], end="")
         except IndexError:
             break
-    i++
+        i += 1
     print("")
     return (i)
