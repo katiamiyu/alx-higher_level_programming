a00e4a7c26dcac2dc038c4e5e346cc61bfaa3dc5
katiamiyu <katiamiyu@gmail.com>
2023-09-11 15:55:57 +0100

    2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-module contains function that checks instamce of class
+module contains function that checks exactly instamce of class
 """
 
 
 def is_same_class(obj, a_class):
-    """ checks if object is instance of class
+    """ checks if object is exactly an instance of class
         Args:
             obj - first argument(instance)
             a_class - class object(blueprint)
     """
-    return isinstance(obj, a_class)
+    return type(obj) is a_class
