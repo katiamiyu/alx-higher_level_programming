87767dda1bb7e1a7aafa72cc949f82a7c8d7433b
katiamiyu <katiamiyu@gmail.com>
2023-09-11 17:08:41 +0100

    7-base_geometry.py

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+module contains an BaseGeometry class
+"""
+
+class BaseGeometry:
+    """ this is a class
+         methods:
+         area
+    """
+
+    def area(self):
+        """ gets area of shape
+            args: void
+            Return: raise exception
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ check type and value of entry
+            Args:
+            name - string
+            value - integer
+        """
+        if not isinstance(int, value):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
