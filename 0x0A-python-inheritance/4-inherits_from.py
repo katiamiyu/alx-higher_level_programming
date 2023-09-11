f1ddd6105646df0d352e87c912aac854341c6f52
katiamiyu <katiamiyu@gmail.com>
2023-09-11 17:55:51 +0100

    1-my_list.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,18 +5,13 @@ module contains function that checks inhertance of class
 
 
 def inherits_from(obj, a_class):
-    """ checks if object is a subclass of class
-        Args:
-            obj - first argument(instance)
-            a_class - class object(blueprint)
+    """
+    checks if object is a subclass of class
     """
     if isinstance(obj, a_class):
         return True
-
     obj_class = obj.__class__
-
     for p_class in obj_class.__bases__:
         if inherits_from(p_class, a_class):
             return True
-
     return False
