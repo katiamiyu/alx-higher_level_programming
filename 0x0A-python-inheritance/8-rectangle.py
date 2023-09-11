24daf537f972773dfd3665d6005e0cb22d4d83f1
katiamiyu <katiamiyu@gmail.com>
2023-09-11 17:39:14 +0100

    8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+module contains Rectangle class
+"""
+
+
+class Rectangle(BaseGeometry):
+    """
+        Rectangle class blueprint
+    """
+    def __init__(self, width, height):
+        """ 
+        init doc
+        """
+        integer_validator("width", width)
+        self.__width = width
+        integer_validator("height", height)
+        self.__height = height
