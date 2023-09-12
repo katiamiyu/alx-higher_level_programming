9d8579aba4dd28704644d2e0b10f486790b1da0c
katiamiyu <katiamiyu@gmail.com>
2023-09-12 17:08:48 +0000

    7-add_item.py

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+add two object files together using json
+"""
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+def add_item():
+    """ add json objects
+        Args:
+            void
+    """
+    try:
+        my_list = load_from_json_file("add_item.json")
+        for i in range(1, len(sys.argv)):
+            my_list.append(sys.argv[i])
+        save_to_json_file(my_list, "add_item.json")
+    except FileNotFoundError:
+        with open("add_item.json", "w") as f:
+            f.write("[]")
+        for i in range(1, len(sys.argv)):
+            my_list.append(sys.argv[i])
+        save_to_json_file(my_list, "add_item.json")
+
+
+if __name__ == "__main__":
+    add_item()
