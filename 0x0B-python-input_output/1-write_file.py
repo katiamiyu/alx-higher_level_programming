69ed649e78c56570c42697ad1b0d59b23f6ab129
katiamiyu <katiamiyu@gmail.com>
2023-09-12 13:14:12 +0100

    1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module defines writefile mrthod
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    opens a file and writes a fixed number of text
+    Args:
+        filename: name of file
+        text: string to be written
+    Return: numbrr of char
+    """
+    with open(filename, "w", encoding = "utf-8") as f:
+        n_of_char = f.write(text)
+        return n_of_char
