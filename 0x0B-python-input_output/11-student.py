b1106e4aced715f78d28c6c2e9c2f53c8be03b14
katiamiyu <katiamiyu@gmail.com>
2023-09-12 19:28:11 +0000

    11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Module contain Student class
+"""
+
+
+class Student:
+    """ class student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """ initialises student class
+        Args:
+            first_name, last_name, age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ convert dictionary to json
+        """
+        if attrs is None:
+            return self.__dict__
+        elif isinstance(attrs, list):
+            class_dict = {}
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    class_dict[key] = value
+            return class_dict
+
+    def reload_from_json(self, json):
+        """ reloads the attribute of class
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
