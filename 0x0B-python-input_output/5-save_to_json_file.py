d9d94e5067192c063e68bd1f76a8e754912275ac
katiamiyu <katiamiyu@gmail.com>
2023-09-12 15:44:13 +0000

    5-save_to_json_file.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -11,5 +11,5 @@ def save_to_json_file(my_obj, filename):
     Args:
         my_obj, filename
     """
-    with open(filename, "w", encoding="utf-8") as f:
+    with open(filename, "w") as f:
         json.dump(my_obj, f)
