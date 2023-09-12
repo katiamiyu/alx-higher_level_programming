392fb5d5c0c2ee3dff7cbd0371a61b3ea42b9406
katiamiyu <katiamiyu@gmail.com>
2023-09-12 19:09:33 +0000

    10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -22,9 +22,9 @@ class Student:
         """
         if attrs is None:
             return self.__dict__
-        else:
+        elif isinstance(attrs, list):
             class_dict = {}
-            for li in attrs:
-                if li in self.__dict__.keys():
-                    class_dict[li] = self.__dict__[li]
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    class_dict[key] = value
             return class_dict
