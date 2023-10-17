c2902a7fc1a634f0659abab940ada2531b38c92c
katiamiyu <katiamiyu@gmail.com>
2023-10-17 16:16:54 +0000

    9-full_creation.sql

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,3 @@
+-- full creation of table
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256);
+INSERT INTO second_table VALUES (1, "John", 10), (2, "Alex", 3), (3, "Bob", 14), (4, "George", 8);
