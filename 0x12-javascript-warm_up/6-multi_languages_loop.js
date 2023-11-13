47fae770cca730dac4ed74f6894b192b7bd10718
katiamiyu <katiamiyu@gmail.com>
2023-11-13 15:50:12 +0000

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
 
+let i;
 let myMusic = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-myMusic.forEach((value) => console.log(`${value}`));
+for(i=0; i<myMusic.length; i++) {
+  console.log(myMusic[i]);
+}
