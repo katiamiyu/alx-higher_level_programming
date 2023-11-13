50c1e5c6956958179556817a18184209527c76d2
katiamiyu <katiamiyu@gmail.com>
2023-11-13 19:16:45 +0000

    101-call_me_moby.js

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 exports.callMeMoby = function (x, theFunction) {
-  for (let k = 0; k < x; k++) {
-    theFunction(k);
+  for (let i = 0; i < x; ++) {
+    theFunction();
   }
 };
