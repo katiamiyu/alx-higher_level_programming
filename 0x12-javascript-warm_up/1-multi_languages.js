15b0d25831b655e3e3b9b23f4cb7efd6861e0d23
katiamiyu <katiamiyu@gmail.com>
2023-11-13 11:36:12 +0000

    1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -5,7 +5,6 @@
  * javascript is amazing
  */
 
-const myVar =`C is fun
-Python is cool
-JavaScript is amazing`
-console.log(myVar);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
