9b55282432cfe81a445239c3ee93a01fbb6d4d49
katiamiyu <katiamiyu@gmail.com>
2023-11-13 11:20:33 +0000

    Readme and task 0

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/* scripts prints javascript is amazing */
+
+let myVar;
+myVar = “JavaScript is amazing”;
+
+console.log(myVar)
---
+++ 0x12-javascript-warm_up/README.md
@@ -0,0 +1,3 @@
+# 0x12-javascript-warm_up
+- Author: katiamiyu
+- Date: 13-11-2023
