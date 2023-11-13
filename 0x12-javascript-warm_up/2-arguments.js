54986edff411f8e75b39ef4e3f5885827d2e3473
katiamiyu <katiamiyu@gmail.com>
2023-11-13 14:02:11 +0000

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-if (process.argv[2] === undefined) {
+if (process.argv[2] == undefined) {
 	console.log('No argument');
-} else if (process.argv[3] === undefined) {
+} else if (process.argv[3] == undefined) {
 	console.log('Argument found');
 } else {
 	 console.log('Arguments found');
