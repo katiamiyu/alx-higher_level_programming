d495a36a7b14644415775b28665bad98a0971972
katiamiyu <katiamiyu@gmail.com>
2023-11-13 16:55:34 +0000

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-let myMusic = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const myMusic = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const music of myMusic) {
   console.log(music);
 }
