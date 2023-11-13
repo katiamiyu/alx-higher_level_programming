439c0726f75e4b11b638f3ece024d5275f4b13de
katiamiyu <katiamiyu@gmail.com>
2023-11-13 11:26:17 +0000

    0-javascript_is_amazing.js

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 /* scripts prints javascript is amazing */
 
-const myVar = “JavaScript is amazing”;
-
-console.log(myVar)
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
