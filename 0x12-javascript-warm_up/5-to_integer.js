7615d59342643b6561e583c50c721a23a6782fd2
katiamiyu <katiamiyu@gmail.com>
2023-11-13 15:58:48 +0000

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,11 +2,11 @@
 
 let argv = process.argv;
 if (argv[2] == null)
-	console.log('Not a number');
+  console.log('Not a number');
 else
 {
-	if (isNaN(argv[2]))
-		console.log('Not a number');
-	else
-		console.log(`My number: ${parseInt(argv[2])}`);
+  if (isNaN(argv[2]))
+    console.log('Not a number');
+  else
+    console.log(`My number: ${parseInt(argv[2])}`);
 }
