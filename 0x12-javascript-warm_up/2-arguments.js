a3b5af5e9b8dff40505b9e53f7559fa0464cd41d
katiamiyu <katiamiyu@gmail.com>
2023-11-13 13:03:47 +0000

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,10 +3,9 @@
 
 let msg;
 const argvL = process.argv.length;
-if (argvL < 3){
+if (argvL < 3) {
 	msg = 'No argument';
-} else if (argvL === 3)
-{
+} else if (argvL === 3) {
 	msg = 'Argument found';
 } else {
 	msg = 'Arguments found';
