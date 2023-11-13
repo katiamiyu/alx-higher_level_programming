c133dba522aedc807a929a5f04b2931ba582cbe3
katiamiyu <katiamiyu@gmail.com>
2023-11-13 15:56:32 +0000

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
+/* print argv number of C is fun */
 
 let i;
 if (isNaN(parseInt(process.argv[2]))) {
-	console.log('Missing number of occurences');
+  console.log('Missing number of occurences');
 } else {
-	for (i = 0; i < parseInt(process.argv[2]); i++) {
-		console.log('C is fun');
-	}
+  for (i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log('C is fun');
+  }
 }
