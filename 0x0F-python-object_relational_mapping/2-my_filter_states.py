16db9db49e2142aba97f9a48fe0529ba188aacf7
katiamiyu <katiamiyu@gmail.com>
2023-11-17 11:58:31 -0800

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+module uses MySQLdb to access database
+'''
+
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         db=sys.argv[3], user=sys.argv[1],
+                         passwd=sys.argv[2])
+    con = db.cursor()
+    con.execute(
+            'SELECT * FROM states WHERE name = "{:s}"\
+            ORDER BY states.id ASC'.format(sys.argv[4])
+    )
+    for row in con.fetchall():
+        print (row)
+
