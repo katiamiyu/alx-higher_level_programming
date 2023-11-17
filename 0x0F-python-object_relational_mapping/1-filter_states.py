4162adb65895f238220d6aa7e2579cd32bd5214a
katiamiyu <katiamiyu@gmail.com>
2023-11-17 08:20:46 -0800

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,6 +11,7 @@ if __name__ == '__main__':
                          db=sys.argv[3], user=sys.argv[1],
                          passwd=sys.argv[2])
     con = db.cursor()
-    con.execute('SELECT * FROM states WHERE name LIKE "N%" ORDER BY states.id')
+    con.execute('SELECT * FROM states ORDER BY states.id')
     for row in con.fetchall():
-        print(row)
+        if row[1].startswith('N'):
+            print(row)
