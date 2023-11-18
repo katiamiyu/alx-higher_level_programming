b8bbcaa4ae62e3199b16e9bf6251137b9b6ce400
katiamiyu <katiamiyu@gmail.com>
2023-11-18 05:20:34 -0800

    8-model_state_fetch_first.py

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+module uses Alchemy to access database
+'''
+
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == '__main__':
+    engine = create_engine(
+             'mysql+mysqldb://{}:{}@localhost:3306/{}'
+             .format(
+                     sys.argv[1],
+                     sys.argv[2],
+                     sys.argv[3],
+                     pool_pre_ping=True
+             )
+             )
+    engine.connect()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).order_by(State.id).first()
+    if state is not None:
+        print(f'{state.id}: {state.name}')
+    session.close()
