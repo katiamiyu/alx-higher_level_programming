f8aa2e249535484677dfb74352303c08072e1ba3
katiamiyu <katiamiyu@gmail.com>
2023-11-18 04:04:12 -0800

    6-model_state.sql

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,15 +4,19 @@ module uses SQLAlchemy to access database
 '''
 
 
-import sys
-from model_state import Base, State
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
 
 
-from sqlalchemy import (create_engine)
+class State(Base):
+    ''' Declaration of mapping to states table
+        inherits from Base
+    '''
 
-if __name__ == '__main__':
-    engine = create_engine(
-             'mysql+mysqldb://{}:{}@localhost/{}'
-             .format(sys.argv[1], sys.argv[2], sys.argv[3], pool_pre_ping=True)
-             )
-    Base.metadata.create_all(engine)
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
