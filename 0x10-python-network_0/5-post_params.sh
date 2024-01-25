d39b0cd644df9fc5e2aea97201423a051e4e9499
katiamiyu <katiamiyu@gmail.com>
2024-01-25 18:16:05 +0000

    5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# a Bash script that takes in a URL, sends a POST request to the passed URL
+# a Bash script takes in a URL, sends a POST request to the supplied URL
 curl -s -d "email=test@gmail.com&subject=I will always be here for PLD" -X POST $1
