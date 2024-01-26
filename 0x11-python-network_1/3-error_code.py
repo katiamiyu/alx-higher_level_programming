39d6eb9db5397682aab2ffd65d67c4d0fc4caa64
katiamiyu <katiamiyu@gmail.com>
2024-01-26 14:02:14 +0000

    10-my_github.py

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Python script that 
+- takes in a URL, sends a request to the URL
+- displays the body of the response (decoded in utf-8).
+"""
+
+if __name__ == "__main__":
+    from urllib import request, error
+    import sys as system
+
+    try:
+        with request.urlopen(system.argv[1]) as r:
+            print(r.read().decode('UTF-8'))
+    except error.HTTPError as e:
+        print("Error code:", e.code)
