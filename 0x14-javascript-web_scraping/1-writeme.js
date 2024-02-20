fc919c341d3d230792d9274af58c80310394b257
katiamiyu <katiamiyu@gmail.com>
2024-02-20 19:11:49 +0100

    0-readme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const argv = process.argv;
+const fs = require('fs');
+fs.writeFile(argv[2], argv[3], 'utf-8', (error) => {
+  if (error) {
+    console.error(error);
+  }
+});
---
+++ 0x14-javascript-web_scraping/README.md
@@ -0,0 +1,3 @@
+# web scraping in javascript
+- author: katiamiyu
+- date: 20-feb-24
